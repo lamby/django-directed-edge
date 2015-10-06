800f88e67ea0049dc306c9d9c003f52484b2d793
Chris Lamb <chris@chris-lamb.co.uk>
2015-10-06 23:20:04 +0200

    update setup.py

--- setup.py
+++ setup.py
@@ -6,4 +6,8 @@ setup(
     packages=(
         'django_directed_edge',
     ),
+    url='https://chris-lamb.co.uk/projects/django-directed-edge',
+    author="Chris Lamb",
+    author_email="chris@chris-lamb.co.uk",
+    description="Helpful Django-oriented sugar around around DirectedEdge's Python API",
 )
